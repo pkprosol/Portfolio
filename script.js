07bf5f6b237885cc958bc279dfa95a980b825fb6
Peter Prosol <peter.prosol@gmail.com>
2014-01-16 15:18:24 -0500

    Show or hide

--- script.js
+++ script.js
@@ -1,36 +1,15 @@
 $(document).ready(function() {
+	$('.categories').hide();
 	$('#header').click(function() {
 		$(this).toggleClass("clicked");
-		$('.categories').toggle(showOrHide);
-		$(this).animate({
-			marginTop: "75px"
-		}, 1000 );
+		$('.categories').toggle();
 	});
+
 //	$('.categories').hide();
 	$('.categories').click(function() {
 		$(this).toggleClass("clicked");
 	});
 
 	$('.content').hide();
-	$('#education').hover(function() {
-		$(this).toggleClass("highlighted");
-		$('.content').hide();
-		$('#educationContent').show();
-	});
-	$('#experience').hover(function() {
-		$(this).toggleClass("highlighted");		
-		$('.content').hide();
-		$('#experienceContent').show();
-	});
-	$('#work').hover(function() {
-		$(this).toggleClass("highlighted");
-		$('.content').hide();
-		$('#workContent').show();
-	});
-	$('#personal').hover(function() {
-		$(this).toggleClass("highlighted");
-		$('.content').hide();
-		$('#personalContent').show();
-	});
 
 });
