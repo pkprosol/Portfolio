b44f0d15143c896ce661996c38737b4e5e972904
Peter Prosol <peter.prosol@gmail.com>
2014-01-16 01:45:51 -0500

    Style

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 $(document).ready(function() {
 	$('.content').hide();
 	$('#education').hover(function() {
+		$(this).addClass("highlighted");
 		$('.content').hide();
 		$('#educationContent').show();
 	});
 	$('#experience').hover(function() {
+		$(this).addClass("highlighted");
 		$('.content').hide();
 		$('#experienceContent').show();
 	});
 	$('#work').hover(function() {
+		$(this).addClass("highlighted");
 		$('.content').hide();
 		$('#workContent').show();
 	});
 	$('#personal').hover(function() {
+		$(this).addClass("highlighted");
 		$('.content').hide();
 		$('#personalContent').show();
 	});
